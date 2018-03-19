862f623cfa3d24c36fd767f8e41a7e57b03f3b85
racheal234 <rachmbugua@gmail.com>
2018-03-19 11:24:51 +0300

    edit component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+quotes: string[];
+
+constructor(){
+
+this.quotes = ["Learning never exhausts the mind","Lord make me an instrument of your peace where there is hatred
+let me sow love","I still think I am the greatest" ];
+}
 }
