54227b47a845b51ca00a2d19072465faf4ca1992
racheal234 <rachmbugua@gmail.com>
2018-03-19 15:14:17 +0300

    add creating component details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
-import {Quote} from './quote'
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
 })
-export class AppComponent {
+export class QuotesComponent implements OnInit {
+
 quotes = [
  new Quote(1, 'I still think I am the greatest','Kanye west'),
  new Quote(2, 'What do I wear in bed? Why Chanel No 5','Marylin Monroe'),
  new Quote(4, 'Learning never exhausts the mind','Socrates'),
  new Quote(5, 'speed so fast i felt like i was drunk','Jonas Blue'),
 ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
