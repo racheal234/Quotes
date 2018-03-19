285913163c24147d2f0a814bb55d55e1b114477a
racheal234 <rachmbugua@gmail.com>
2018-03-19 12:53:23 +0300

    generate a new quote.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-quotes: string[];
+quotes = [
+ new Quote(1, 'I still think I am the greatest'),
+ new Quote(2, 'What do I wear in bed? Why Chanel No 5'),
+ new Quote(4, 'Learning never exhausts the mind'),
+ new Quote(5, 'speed so fast i felt like i was drunks'),
+]
 
-constructor(){
-
-this.quotes = ["Learning never exhausts the mind","Lord make me an instrument of your peace where there is hatred
-let me sow love","I still think I am the greatest" ];
-}
 }
